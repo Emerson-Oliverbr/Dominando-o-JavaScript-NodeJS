721f5aa90405441ef502a06a5420ed9d9ba9547f
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-06-02 11:49:04 -0300

    iniciando Modulo-2 Tipos Primitivos e Datas

---
+++ Modulo-2 Tipos de dados primitivos e Datas/tiposPrimitivos.js
@@ -0,0 +1,11 @@
+const v1 = 5;
+const v2 = 2.65;
+const v3 = true;
+const nome = "Emerson";
+const sobreNome = "Emerson";
+
+console.log(typeof(v1)); // -> number
+console.log(typeof(v2)); // -> number
+console.log(typeof(v3)); // -> boolean
+console.log(typeof(nome)); // -> string
+console.log(typeof(sobreNome)); // -> string
