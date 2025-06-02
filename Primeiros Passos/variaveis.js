f60f07177e26f44007e33c3f97c72f002903d319
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-06-02 10:42:33 -0300

    Definindo variaveis e armazenando valores

---
+++ Primeiros Passos/variaveis.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 6;
+let c;
+let d;
+
+c = 10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+let resultado = (a + b + c);
+
+console.log("O resultado de " + a + "+" + b + "+" + c + "=" + resultado);
+
